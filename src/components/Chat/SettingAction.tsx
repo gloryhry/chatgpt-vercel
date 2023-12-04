bd6cfa98c9561893248585e7b9b67efef2a9d539
GloryHRY <glory@hzbz.edu.eu.org>
2023-12-04 16:55:09 +0800

    add multi gpt models

--- src/components/Chat/SettingAction.tsx
+++ src/components/Chat/SettingAction.tsx
@@ -142,16 +142,36 @@ export default function SettingAction() {
                 }}
                 options={[
                   {
-                    value: "gpt-3.5",
+                    value: "gpt-3.5-turbo",
                     label: "gpt-3.5(auto)"
+                  },
+                  {
+                    value: "gpt-3.5-turbo-0613",
+                    label: "gpt-3.5-turbo-0613"
+                  },
+                  {
+                    value: "gpt-3.5-turbo-1106",
+                    label: "gpt-3.5-turbo-1106"
+                  },
+                  {
+                    value: "gpt-3.5-turbo-16k",
+                    label: "gpt-3.5-turbo-16k"
+                  },
+                  {
+                    value: "gpt-3.5-turbo-16k-0613",
+                    label: "gpt-3.5-turbo-16k-0613"
                   },
                   {
                     value: "gpt-4",
                     label: "gpt-4(auto)"
                   },
                   {
-                    value: "gpt-4-preview",
-                    label: "gpt-4(preview)"
+                    value: "gpt-4-0613",
+                    label: "gpt-4-0613"
+                  },
+                  {
+                    value: "gpt-4-1106-preview",
+                    label: "gpt-4-1106-preview"
                   }
                 ]}
               />
